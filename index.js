13ce810028bcafc3616d3ce9f54a93f75f2ddff5
Onuorah Ike <ikecodes@gmail.com>
2022-03-01 04:54:51 +0100

    added comments to listeners and updated port

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const io = require('socket.io')(8000, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: '*',
   },
 });
+//  origin: 'http://localhost:3000',
 
+////This will contain all drivers currently available with there locations
 let drivers = [];
 
 const addDriver = (data) => {
@@ -17,35 +19,42 @@ const removeDriver = (socketId) => {
 };
 
 const getDriver = (driverId) => {
-  return drivers.find((driver) => driver.id == driverId);
+  return drivers.find((driver) => driver.id === driverId);
 };
 
 io.on('connection', (socket) => {
   ///Add online riders and update locations (drivers action)
+  ///You will emit addDriver event here. Once a driver is online, he should get connected to the websocket.
   socket.on('addDriver', (driver) => {
     const socketId = socket.id;
     addDriver({ ...driver, socketId });
   });
 
   ///Get all online drivers (customers action)
+  ///When a customer wants to make a delivery, you will emit a getDrivers event so I can send you a list of all drivers online
   socket.on('getDrivers', () => {
     socket.emit('driversList', drivers);
   });
 
   ///Select a driver (customers action)
+  ///From the list of drivers you have, the customer will select a driver and send a pickup request to that driver.
   socket.on('selectDriver', (driversId, customerInfo) => {
     const driver = getDriver(driversId);
     if (driver) {
+      ///make sure the customerInfo here also contains the customers socketId because you will respond back with it
       io.to(driver.socketId).emit('pickupRequest', customerInfo);
     }
   });
 
   ///Pickup request action (drivers action)
+  ///Here the driver responds to the pickupRequest, either accepting or rejecting.
   socket.on('pickupReply', (customersId, answer) => {
+    ///responding back to the customer with his socketid
     io.to(customersId).emit('answer', answer);
   });
 
   ////Get driver location
+  ///whenever the user wants to get the driver with the goods current location
   socket.on('getDriver', (driverId) => {
     const driver = getDriver(driverId);
     socket.emit('location', driver);
